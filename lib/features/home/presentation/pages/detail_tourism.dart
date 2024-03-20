f6b662962cf68124e2d5ef8a1b3c4d8fd7a13716
SiTelur <70305841+SiTelur@users.noreply.github.com>
2024-03-20 08:26:04 +0700

    Update detail_tourism.dart

--- lib/features/home/presentation/pages/detail_tourism.dart
+++ lib/features/home/presentation/pages/detail_tourism.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:kembang_belor_apps/features/home/domain/entities/tourism.dart';
 
 class DetailTourims extends StatelessWidget {
@@ -9,16 +12,89 @@ class DetailTourims extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(entity.name!),
-      ),
-      body: Column(
-        children: [
-          CachedNetworkImage(
-            cacheKey: '/tourism',
-            imageUrl: entity.imageUrl!,
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Stack(children: [
+              CachedNetworkImage(
+                cacheKey: '/tourism',
+                imageBuilder: (context, imageProvider) => Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: imageProvider, fit: BoxFit.cover)),
+                ),
+                imageUrl: entity.imageUrl!,
+              ),
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  )),
+            ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    entity.name!,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: Text('')),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0, right: 20),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return SizedBox(
+                            height: 200,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  const Text('Modal BottomSheet'),
+                                  ElevatedButton(
+                                    child: const Text('Close BottomSheet'),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                ],
+                              ),
+                            ));
+                      },
+                    );
+                  },
+                  label: Icon(
+                    Icons.navigate_next,
+                    size: 40,
+                  ),
+                  icon: Text(
+                    'Pesan Sekarang',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
